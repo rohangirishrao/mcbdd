2c304d94de4447e3a266a840f8989ebe6febfd8a
Rohan Girish <rohangirishrao@gmail.com>
2022-05-03 14:57:14 +0200

    Remove redundant python file

--- module2.py
+++
@@ -1,8 +0,0 @@
-import IPython.core.display_functions
-from chembl_webresource_client.new_client import new_client
-import pandas as pd
-
-molecule = new_client.molecule
-approved_drugs = molecule.filter(first_approval_isnull=False).filter(max_phase=4)
-ad_df = pd.DataFrame(approved_drugs)
-IPython.core.display_functions.display(ad_df)
